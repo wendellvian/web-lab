4ff44defa99f15fbafe46f4d45143721cb7c619a
wendellvian <ybyqlong@126.com>
2014-01-15 19:41:11 +0800

    new

---
+++ jsstudy/move/move.js
@@ -0,0 +1,43 @@
+// 获取元素内部样式
+function getStyle(obj, attr){
+	if(obj.currentStyle){
+		return obj.currentStyle[attr];
+	}else{
+		return getComputedStyle(obj, false)[attr];
+	}
+}
+
+// 获取内部样式及设置样式
+function css(obj, attr, value){
+	if(arguments.length == 2){
+		return getStyle(obj, attr);
+	}else if(arguments.length == 3){
+		return obj.style[attr] = value;
+	}
+}
+
+
+
+// 运动框架
+function startMove(obj, attr, iTarget){
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function(){
+		var iCur = 0;
+		if(attr == "opacity"){
+			iCur = parseInt(parseFloat(getStyle(obj,attr))*100);
+		}else{
+			iCur = parseInt(getStyle(obj,attr));
+		}
+		var iSpeed = (iTarget-iCur)/8;
+		iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
+		if(iCur == iTarget){
+			clearInterval(obj.timer);
+		}else{
+			if(attr == "opacity"){
+				obj.style.filter = "alpha(opacity="+(iCur+iSpeed)+")";
+				obj.style.opacity = (iCur+iSpeed)/100;
+			}
+			obj.style[attr] = iCur + iSpeed + "px";
+		}
+	},30);
+}
